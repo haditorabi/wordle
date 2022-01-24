d0892dcc5f584c6f0cfa7190c5554514e1877b7e
haditbfbs22 <haditbfbs22@gmail.com>
2022-01-23 22:39:37 -0500

    Added farsi font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      body: ['Dana', 'sans-serif'],
+      display: ['Dana', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
