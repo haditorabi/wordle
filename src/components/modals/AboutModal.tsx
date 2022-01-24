c1560dbe707ad30f20ffff16a2b041cb50d76373
haditbfbs22 <haditbfbs22@gmail.com>
2022-01-23 22:02:00 -0500

    Update about project modal

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,19 +9,19 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500">
-        This is an open source clone of the game Wordle -{' '}
+        This is a project forked from -{' '}
         <a
-          href="https://github.com/hannahcode/wordle"
+          href="https://github.com/haditorabi/wordle"
           className="underline font-bold"
         >
           check out the code here
         </a>{' '}
         and{' '}
         <a
-          href="https://www.powerlanguage.co.uk/wordle/"
+          href="https://github.com/hannahcode/wordle/"
           className="underline font-bold"
         >
-          play the original here
+          see the main project here
         </a>
       </p>
     </BaseModal>
